68ad9ecfb32cf64accd53631f9902bd2468cb747
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2024-06-19 22:01:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
